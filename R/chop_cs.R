3eef63ee4fbbb80d76b1531dba8e0515fc6b9ebb
AngelosPsy <amkrypotos@gmail.com>
2020-03-25 12:34:59 +0100

    Fixed renaming in chop_cs

--- R/chop_cs.R
+++ R/chop_cs.R
@@ -9,8 +9,10 @@
 #' @export
 
 chop_cs <- function(cs, data, subj, na.rm = FALSE) {
+
   cs <- data %>% dplyr::select(all_of(!!dplyr::enquo(cs)))
-  subj <- data %>% dplyr::select(all_of(!!dplyr::enquo(subj)))
+  subj <- data %>% dplyr::select(all_of(!!dplyr::enquo(subj))) %>%
+    dplyr::rename(id = 1)
 
   # Determine how many trials you have. Based on that, you will determine
   # how many parts you will have to make.
@@ -20,29 +22,38 @@ chop_cs <- function(cs, data, subj, na.rm = FALSE) {
   if ((nc_cs %% 2) == 0) {
     # Half of the trials
     cs_fhalf <-
-      data %>% dplyr::select(1, all_of(nc_cs) / 2) %>% rowMeans(na.rm = na.rm) %>%
+      data %>% dplyr::select(1, all_of(nc_cs) / 2) %>%
+      rowMeans(na.rm = na.rm) %>%
       tibble::enframe(name = NULL, value = "cs_fhalf")
     cs_lhalf <-
-      data %>% dplyr::select(all_of(nc_cs) / 2 + 1, all_of(nc_cs)) %>% rowMeans(na.rm = na.rm) %>%
+      data %>% dplyr::select(all_of(nc_cs) / 2 + 1, all_of(nc_cs)) %>%
+      rowMeans(na.rm = na.rm) %>%
       tibble::enframe(name = NULL, value = "cs_lhalf")
   } else{
     # You do the nc_cs + 1 trick because it is an odd number. Have to think
     # more of that
     cs_fhalf <-
-      data %>% dplyr::select(1, (all_of(nc_cs) + 1) / 2) %>% rowMeans(na.rm = na.rm) %>%
+      data %>% dplyr::select(1, (all_of(nc_cs) + 1) / 2) %>%
+      rowMeans(na.rm = na.rm) %>%
       tibble::enframe(name = NULL, value = "cs_fhalf")
     cs_lhalf <-
-      data %>% dplyr::select((all_of(nc_cs) + 1) / 2 + 1, all_of(nc_cs)) %>% rowMeans(na.rm = na.rm) %>%
+      data %>% dplyr::select((all_of(nc_cs) + 1) / 2 + 1, all_of(nc_cs)) %>%
+      rowMeans(na.rm = na.rm) %>%
       tibble::enframe(name = NULL, value = "cs_lhalf")
   }
 
   # Just the last trial and the last two trials
-  cs_ltrial <- cs %>% dplyr::select(utils::tail(names(.), 1)) %>% tibble::enframe(name = NULL, value = "cs_ltrial")
-  cs_l2trial <- cs %>% dplyr::select(utils::tail(names(.), 2))
+  cs_ltrial <-
+    cs %>% dplyr::select(utils::tail(names(.), 1)) %>%
+    unlist() %>% tibble::enframe(name = NULL, value = "cs_ltrial")
+  cs_l2trial <-
+    cs %>% dplyr::select(utils::tail(names(.), 2)) %>%
+    dplyr::rename(l1trial = 1, l2trial = 2)
 
   # Difference between last and first trial
   cs_lftdiff <-
-    cs %>% dplyr::select(1, utils::tail(names(.), 1)) %>% apply(1, diff) %>%
+    cs %>% dplyr::select(1, utils::tail(names(.), 1)) %>%
+    apply(1, diff) %>%
     tibble::enframe(name = NULL, value = "cs_lftdiff")
 
   cs_half  <- dplyr::bind_cols(cs_fhalf, cs_lhalf)
