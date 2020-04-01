b5c721401f28da7c328311492c3d9376242a9cd8
AngelosPsy <amkrypotos@gmail.com>
2020-04-01 21:10:22 +0200

    Corrected multiple bugs that did not compute correct the first and last trials

--- R/chop_cs.R
+++ R/chop_cs.R
@@ -10,7 +10,7 @@
 
 chop_cs <- function(cs, data, subj, na.rm = FALSE) {
   # Check data
-  collection_warning(cs1 = cs1, data = data, subj = subj)
+  collection_warning(cs1 = cs, data = data, subj = subj)
 
   cs <- data %>% dplyr::select(all_of(!!dplyr::enquo(cs)))
   subj <- data %>% dplyr::select(all_of(!!dplyr::enquo(subj))) %>%
@@ -19,27 +19,31 @@ chop_cs <- function(cs, data, subj, na.rm = FALSE) {
   # Determine how many trials you have. Based on that, you will determine
   # how many parts you will have to make.
   nc_cs <- ncol(cs)
+  fh <- nc_cs / 2
+  fhp1 <- (nc_cs / 2) + 1
+  fhp1Odd <-  ((nc_cs + 1) / 2)
+  fhp2Odd <-  fhp1Odd + 1
 
   # Split data in 2 halfs
   if ((nc_cs %% 2) == 0) {
     # Half of the trials
     cs_fhalf <-
-      data %>% dplyr::select(1, all_of(nc_cs) / 2) %>%
+      cs %>% dplyr::select(1:fh) %>%
       rowMeans(na.rm = na.rm) %>%
       tibble::enframe(name = NULL, value = "cs_fhalf")
     cs_lhalf <-
-      data %>% dplyr::select(all_of(nc_cs) / 2 + 1, all_of(nc_cs)) %>%
+      cs %>% dplyr::select(fhp1:nc_cs) %>%
       rowMeans(na.rm = na.rm) %>%
       tibble::enframe(name = NULL, value = "cs_lhalf")
-  } else{
+  } else {
     # You do the nc_cs + 1 trick because it is an odd number. Have to think
     # more of that
     cs_fhalf <-
-      data %>% dplyr::select(1, (all_of(nc_cs) + 1) / 2) %>%
+      data %>% dplyr::select(1:fhp1Odd) %>%
       rowMeans(na.rm = na.rm) %>%
       tibble::enframe(name = NULL, value = "cs_fhalf")
     cs_lhalf <-
-      data %>% dplyr::select((all_of(nc_cs) + 1) / 2 + 1, all_of(nc_cs)) %>%
+      data %>% dplyr::select(fhp2Odd:nc_cs) %>%
       rowMeans(na.rm = na.rm) %>%
       tibble::enframe(name = NULL, value = "cs_lhalf")
   }
@@ -58,10 +62,11 @@ chop_cs <- function(cs, data, subj, na.rm = FALSE) {
     apply(1, diff) %>%
     tibble::enframe(name = NULL, value = "cs_lftdiff")
 
-  cs_half  <- dplyr::bind_cols(cs_fhalf, cs_lhalf)
+  cs_half <- dplyr::bind_cols(cs_fhalf, cs_lhalf)
 
   res <-
-    dplyr::bind_cols(subj, cs_ltrial, cs_l2trial, cs_lftdiff, cs_half) %>% tidyr::as_tibble()
+    dplyr::bind_cols(subj, cs_ltrial, cs_l2trial, cs_lftdiff, cs_half) %>%
+    tidyr::as_tibble()
 
   return(res)
 }
