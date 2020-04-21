c5a0f658b35cf5ffe938a2f0a15a69e9aacf8a1b
AngelosPsy <amkrypotos@gmail.com>
2020-04-21 17:24:23 +0200

    Documented data

---
+++ R/data.R
@@ -0,0 +1,30 @@
+#' Simulated data sets of skin conductance responses
+#'
+#' A simulated data set containing conditioned skin conductance responses for
+#' 10 conditioned stimulus plus trials and a equal number of conditioned stimulus minus trials The variables are as follows:
+#'
+#' @format A data frame with 40 rows and 21 variables:
+#' \describe{
+#'   \item{CSP1}{conditioned stimulus plus, trial 1}
+#'   \item{CSP2}{conditioned stimulus plus, trial 2}
+#'   \item{CSP3}{conditioned stimulus plus, trial 3}
+#'   \item{CSP4}{conditioned stimulus plus, trial 4}
+#'   \item{CSP5}{conditioned stimulus plus, trial 5}
+#'   \item{CSP6}{conditioned stimulus plus, trial 6}
+#'   \item{CSP7}{conditioned stimulus plus, trial 7}
+#'   \item{CSP8}{conditioned stimulus plus, trial 8}
+#'   \item{CSP9}{conditioned stimulus plus, trial 9}
+#'   \item{CSP10}{conditioned stimulus plus, trial 10}
+#'   \item{CSM1}{conditioned stimulus minus, trial 1}
+#'   \item{CSM2}{conditioned stimulus minus, trial 2}
+#'   \item{CSM3}{conditioned stimulus minus, trial 3}
+#'   \item{CSM4}{conditioned stimulus minus, trial 4}
+#'   \item{CSM5}{conditioned stimulus minus, trial 5}
+#'   \item{CSM6}{conditioned stimulus minus, trial 6}
+#'   \item{CSM7}{conditioned stimulus minus, trial 7}
+#'   \item{CSM8}{conditioned stimulus minus, trial 8}
+#'   \item{CSM9}{conditioned stimulus minus, trial 9}
+#'   \item{CSM10}{conditioned stimulus minus, trial 10}
+#'   \item{id}{Participant number}
+#' }
+"example_data"
