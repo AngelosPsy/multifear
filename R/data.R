4f25a371f9ffb65856e0737789accd5bc4ca757f
AngelosPsy <amkrypotos@gmail.com>
2020-04-29 15:11:53 +0200

    Updated the data so as to include also group

--- R/data.R
+++ R/data.R
@@ -1,30 +1,32 @@
 #' Simulated data sets of skin conductance responses
 #'
 #' A simulated data set containing conditioned skin conductance responses for
-#' 10 conditioned stimulus plus trials and a equal number of conditioned stimulus minus trials The variables are as follows:
+#' 10 conditioned stimulus plus trials and a equal number of conditioned
+#' stimulus minus trials The variables are as follows:
 #'
-#' @format A data frame with 40 rows and 21 variables:
+#' @format A data frame with 40 rows and 22 variables:
 #' \describe{
-#'   \item{CSP1}{conditioned stimulus plus, trial 1}
-#'   \item{CSP2}{conditioned stimulus plus, trial 2}
-#'   \item{CSP3}{conditioned stimulus plus, trial 3}
-#'   \item{CSP4}{conditioned stimulus plus, trial 4}
-#'   \item{CSP5}{conditioned stimulus plus, trial 5}
-#'   \item{CSP6}{conditioned stimulus plus, trial 6}
-#'   \item{CSP7}{conditioned stimulus plus, trial 7}
-#'   \item{CSP8}{conditioned stimulus plus, trial 8}
-#'   \item{CSP9}{conditioned stimulus plus, trial 9}
-#'   \item{CSP10}{conditioned stimulus plus, trial 10}
-#'   \item{CSM1}{conditioned stimulus minus, trial 1}
-#'   \item{CSM2}{conditioned stimulus minus, trial 2}
-#'   \item{CSM3}{conditioned stimulus minus, trial 3}
-#'   \item{CSM4}{conditioned stimulus minus, trial 4}
-#'   \item{CSM5}{conditioned stimulus minus, trial 5}
-#'   \item{CSM6}{conditioned stimulus minus, trial 6}
-#'   \item{CSM7}{conditioned stimulus minus, trial 7}
-#'   \item{CSM8}{conditioned stimulus minus, trial 8}
-#'   \item{CSM9}{conditioned stimulus minus, trial 9}
-#'   \item{CSM10}{conditioned stimulus minus, trial 10}
+#'   \item{CSP1}{Conditioned stimulus plus, trial 1}
+#'   \item{CSP2}{Conditioned stimulus plus, trial 2}
+#'   \item{CSP3}{Conditioned stimulus plus, trial 3}
+#'   \item{CSP4}{Conditioned stimulus plus, trial 4}
+#'   \item{CSP5}{Conditioned stimulus plus, trial 5}
+#'   \item{CSP6}{Conditioned stimulus plus, trial 6}
+#'   \item{CSP7}{Conditioned stimulus plus, trial 7}
+#'   \item{CSP8}{Conditioned stimulus plus, trial 8}
+#'   \item{CSP9}{Conditioned stimulus plus, trial 9}
+#'   \item{CSP10}{Conditioned stimulus plus, trial 10}
+#'   \item{CSM1}{Conditioned stimulus minus, trial 1}
+#'   \item{CSM2}{Conditioned stimulus minus, trial 2}
+#'   \item{CSM3}{Conditioned stimulus minus, trial 3}
+#'   \item{CSM4}{Conditioned stimulus minus, trial 4}
+#'   \item{CSM5}{Conditioned stimulus minus, trial 5}
+#'   \item{CSM6}{Conditioned stimulus minus, trial 6}
+#'   \item{CSM7}{Conditioned stimulus minus, trial 7}
+#'   \item{CSM8}{Conditioned stimulus minus, trial 8}
+#'   \item{CSM9}{Conditioned stimulus minus, trial 9}
+#'   \item{CSM10}{Conditioned stimulus minus, trial 10}
 #'   \item{id}{Participant number}
+#'   \item{group}{Group assignment}
 #' }
 "example_data"
